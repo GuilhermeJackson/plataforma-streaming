4326f028587ae2a083cf963f8cb5dc4bcac7f871
guilherme <guilherme.lamim96@gmail.com>
2022-12-07 14:03:34 -0300

    template sidebar criada

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,20 @@
+import { Lesson } from "./Lesson";
+
 export function Sidebar() {
     return (
-        <div>Sidebar</div>
+        <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+            <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+                Cronograma de aulas
+            </span>
+            <div className="flex flex-col gap-8">
+                <Lesson/>
+                <Lesson/>
+                <Lesson/>
+                <Lesson/>
+                <Lesson/>
+                <Lesson/>
+                <Lesson/>
+            </div>
+        </aside>
     )
 }

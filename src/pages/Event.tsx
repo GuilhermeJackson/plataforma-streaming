ffb1c620f144e3140114717bb1d8c1a828fc04c4
guilherme <guilherme.lamim96@gmail.com>
2022-12-08 13:25:19 -0300

    Criado tamplate para página principal da plataforma

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,5 +1,15 @@
+import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+import { Video } from "../components/Video";
+
 export function Event() {
     return (
-        <div>Event</div>
+        <div>
+            <Header/>
+            <main className="flex">
+                <Video/>
+                <Sidebar/>
+            </main>
+        </div>
     )
 }

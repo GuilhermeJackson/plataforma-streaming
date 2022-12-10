7d06053325acf836d1c3116d066b49510c7fe7f5
guilherme <guilherme.lamim96@gmail.com>
2022-12-10 17:58:39 -0300

    bg-green do lesson selecionado via useParams

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -2,13 +2,17 @@ import { CheckCircle, Lock } from 'phosphor-react'
 import { LessonProps } from '../data/LessonProps'
 import { isPast, format } from "date-fns";
 import ptBR from 'date-fns/locale/pt-BR';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export function Lesson(props: LessonProps) {
+    const { slug } = useParams<{slug: string}>()
+
     const isLessonAvailable = isPast(props.availableAt);
     const availableAtFormatted = format(props.availableAt, "EEEE' • ' d 'de'MMMM' • 'k'h'mm", {
         locale: ptBR
     })
+
+    const isActiveLesson = slug === props.slug
 
     return (
         <Link to={`/event/lesson/${props.slug}`} className='group'>
@@ -16,7 +20,7 @@ export function Lesson(props: LessonProps) {
                 {availableAtFormatted}
             </span>
 
-            <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+            <div className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${isActiveLesson ? 'bg-green-500' : ''}` }>
                 <header className="flex items-center justify-between">
                     {isLessonAvailable ? (
                         <span className="text-sm text-blue-500 font-medium flex items-center gap-2">

fd6baa741fb20cfc9e682ba0555741133746973e
guilherme <guilherme.lamim96@gmail.com>
2022-12-12 18:03:19 -0300

    Carregando video ao se inscrever

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,18 +1,23 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
+import { useGetLessonFirstBySlugQuery } from "../graphql/generated";
 
 export function Event() {
     const { slug } = useParams<{slug: string}>()
-
+    const { data } = useGetLessonFirstBySlugQuery()
+    let firstScreen = ''
+    data?.lessons.map(lesson => {
+        firstScreen = lesson.slug
+    })
+    
     return (
         <div className="flex flex-col min-h-screen">
             <Header/>
             <main className="flex flex-1">
-                { slug
-                ? <Video lessonSlug={slug} /> 
-                : <div className="flex-1" /> }
+                <Video lessonSlug={!slug ? firstScreen : slug} /> 
                 <Sidebar/>
             </main>
         </div>

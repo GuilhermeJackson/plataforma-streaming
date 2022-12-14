bf9f39fe365eccbe0f9e7eb12bda2aa75bf69526
guilherme <guilherme.lamim96@gmail.com>
2022-12-14 11:26:49 -0300

    Responsividade na tela de inscrição

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -23,8 +23,8 @@ export function Subscribe() {
 
     return (
         <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-                <div className="max-w-[640px] ">
+            <div className="px-0 flex flex-col md:flex-row items-center justify-between mt-20 md:px-6 mx-auto">
+                <div className="px-5 text-sm text-center md:text-left md:px-5  max-w-[640px] ">
                     <Logo/>
                     <h1 className="mt-8 text-[2.5rem] leading-tight">
                         Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>
@@ -33,7 +33,7 @@ export function Subscribe() {
                         Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
                     </p>
                 </div>
-                <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+                <div className="w-full p-6 mt-6 md:p-8 bg-gray-700 border border-gray-500 rounded">
                     <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
                     <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
